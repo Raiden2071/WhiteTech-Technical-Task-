71ad3d31833b6f4967681b35496f416776a71188
slava-danilchak <danilchakv@gmail.com>
2025-04-10 15:52:18 +0300

    Remove unneeded interface

--- src/app/feature/books/models/book.model.ts
+++ src/app/feature/books/models/book.model.ts
@@ -5,16 +5,13 @@ export const BOOKS_DIALOG_TYPES = {
 };
 export type BooksDialogType = typeof BOOKS_DIALOG_TYPES[keyof typeof BOOKS_DIALOG_TYPES];
 
-export interface Book extends BookForm {
+export interface Book {
     id: string;
-}
-
-export interface BookForm {
-  name: string;
-  author: string;
-  year: number;
-  description: string;
-  image: string;
+    name: string;
+    author: string;
+    year: number;
+    description: string;
+    image: string;
 }
 
 export interface BooksDialog {
